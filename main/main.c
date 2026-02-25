a1088a49bcbd8ff65ff2127c457d5739a162b3c4
hungd-union <dangd@union.edu>
2026-02-25 14:33:24 -0500

    update function

--- main/main.c
+++ main/main.c
@@ -9,26 +9,96 @@
 #include "esp_adc/adc_oneshot.h"
 #include "driver/ledc.h"
 
-#define LEDC_TIMER      LEDC_TIMER_0
-#define LEDC_MODE       LEDC_LOW_SPEED_MODE
-#define LEDC_OUTPUT_IO      (16)        // pwm signal to motor pin 16
-#define LEDC_CHANNEL    LEDC_CHANNEL_0
-#define LEDC_DUTY_RES   LEDC_TIMER_13_BIT // set duty resolution to 13 bits
+//Global variables
+//Pins declaration
+#define Elevator1           
+#define Elevator2  
+#define Elevator3
+#define Floor1_call
+#define Floor2_callup
+#define Floor2_calldown
+#define Floor3_call
+#define am2302
+#define buzzer
+//LCD pins are inside the LCD init function
 
-
-//Set the PWM signal frequency required by servo motor
+//LEDC configuration
+#define LEDC_TIMER          LEDC_TIMER_0
+#define LEDC_MODE           LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO      (16)        //GPIO for the LEDC .
+#define LEDC_CHANNEL        LEDC_CHANNEL_0
+#define LEDC_DUTY_RES       LEDC_TIMER_13_BIT 
 #define LEDC_FREQUENCY      (50) // Frequency in Hertz.
+#define go_up               ()
+#define go_down             ()
 
-// declare function for initializing ledc
+//LCD structure, including GPIOs
+hd44780_t lcd = {
+    .write_cb = NULL,
+    .font = HD44780_FONT_5X8,
+    .lines = 2,
+    .pins = {
+        .rs = GPIO_NUM_8,                           // GPIO for Register Select
+        .e  = GPIO_NUM_3,                           // GPIO for enable
+        .d4 = GPIO_NUM_9,                           // GPIO for data 4
+        .d5 = GPIO_NUM_10,                          // GPIO for data 5
+        .d6 = GPIO_NUM_11,                          // GPIO for data 6
+        .d7 = GPIO_NUM_12,                          // GPIO for data 7
+        .bl = HD44780_NOT_USED
+    }
+};
+
+//Global boolean values
+
+//function prototypes
+static void pinConfig(void);  
 static void ledc_initialize(void);
+static void input_task(void);
+static void servo_task(void);
+static void floor_logic(void);
 
 void app_main(void)
 {
 
 }
 
+
+void pinConfig(void){
+    //Reset pins
+    gpio_reset_pin(Elevator1);
+    gpio_reset_pin(Elevator2);
+    gpio_reset_pin(Elevator3);
+    gpio_reset_pin(Floor1_call);
+    gpio_reset_pin(Floor2_calldown);
+    gpio_reset_pin(Floor2_callup);
+    gpio_reset_pin(Floor3_call);
+    gpio_reset_pin(am2302);
+    gpio_reset_pin(buzzer);
+
+    //Set directions
+    gpio_set_direction(Elevator1);
+    gpio_set_direction(Elevator2);
+    gpio_set_direction(Elevator3);
+    gpio_set_direction(Floor1_call);
+    gpio_set_direction(Floor2_calldown);
+    gpio_set_direction(Floor2_callup);
+    gpio_set_direction(Floor3_call);
+    gpio_set_direction(am2302);
+    gpio_set_direction(buzzer);
+
+    //Configure pullup/down
+    gpio_pulldown_en(Elevator1);
+    gpio_pulldown_en(Elevator2);
+    gpio_pulldown_en(Elevator3);
+    gpio_pulldown_en(Floor1_call);
+    gpio_pulldown_en(Floor2_calldown);
+    gpio_pulldown_en(Floor2_callup);
+    gpio_pulldown_en(Floor3_call);
+}
+
+
 // function to configure and initialize ledc
-static void ledc_initialize(void)
+void ledc_initialize(void)
 {
     // Prepare and then apply the LEDC PWM timer configuration
     ledc_timer_config_t ledc_timer = {
